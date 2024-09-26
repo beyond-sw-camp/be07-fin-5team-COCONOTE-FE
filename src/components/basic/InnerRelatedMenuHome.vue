01bf36b24fbade31382f355c1b3cc9de1cd74d98
JUNG-EUN1997 <rro0628@gmail.com>
2024-09-26 17:56:29 +0900

    [FEAT] HC-138 채널 명도 store에 입력하도록 변경

--- src/components/basic/InnerRelatedMenuHome.vue
+++ src/components/basic/InnerRelatedMenuHome.vue
@@ -8,7 +8,7 @@
       <v-list-item
         title="공지채널"
         value="1"
-        @click="goToThread('1')"
+        @click="goToThread('1', '공지채널')"
         :class="{ 'selected-item': selectedMenuId === '1' }"
         class="channel-item"
       ></v-list-item>
@@ -16,14 +16,14 @@
       <v-list-item
         title="채널2"
         value="2"
-        @click="goToThread('2')"
+        @click="goToThread('2', '채널2')"
         :class="{ 'selected-item': selectedMenuId === '2' }"
       ></v-list-item>
 
       <v-list-item
         title="채널3"
         value="3"
-        @click="goToThread('3')"
+        @click="goToThread('3','채널3')"
         :class="{ 'selected-item': selectedMenuId === '3' }"
       ></v-list-item>
     </v-list>
@@ -32,15 +32,16 @@
 
 
 <script>
-import { mapActions } from 'vuex';
+import { mapActions } from "vuex";
 
 export default {
   name: "InnerRelatedMenuHome",
   components: {},
   created() {
     this.selectedMenuId = this.$store.getters.getChannelId;
-    this.selectedMenuId = (this.selectedMenuId != null) ? this.selectedMenuId : 'l'; // 기본값으로 일단 1번 선택
-    console.log("초기 채널 아이디 >> ",this.selectedMenuId)
+    this.selectedMenuId =
+      this.selectedMenuId != null ? this.selectedMenuId : "l"; // 기본값으로 일단 1번 선택
+    console.log("초기 채널 아이디 >> ", this.selectedMenuId);
   },
   data() {
     return {
@@ -48,18 +49,19 @@ export default {
       selectedMenuId: "1", // 기본값으로 일단 1번 선택
     };
   },
-  methods:{
-    ...mapActions(['setChannelInfoActions']),
-    goToThread(channelValue) {
+  methods: {
+    ...mapActions(["setChannelInfoActions"]),
+    goToThread(channelValue, channelName) {
       this.setChannelInfoActions(channelValue); // Vuex store에 업데이트
+      this.setChannelNameInfo(channelName); // Vuex store에 업데이트
       this.$router.push(`/${channelValue}/thread`);
 
-      console.log("@@@@@@@@@@@@",this.$store.getters.getChannelId)
+      console.log("@@@@@@@@@@@@", this.$store.getters.getChannelId);
     },
-  }
+  },
 };
 </script>
 
 <style lang="scss" scoped>
-  @import "@/assets/css/innerRelated.scss"; /* SCSS 파일을 불러오기 */
+@import "@/assets/css/innerRelated.scss"; /* SCSS 파일을 불러오기 */
 </style>
--- src/store/channel.js
+++ src/store/channel.js
@@ -1,6 +1,7 @@
 function initState(){
     return {
-        channelId : null //현 접속한 channelId
+        channelId : null, //현 접속한 channelId
+        channelName : null,
     };
 }
 
@@ -9,15 +10,22 @@ const channel = {
     mutations: {
         setChannelInfo(state, channelValue) {
             state.channelId = channelValue;
+        },
+        setChannelNameInfo(state, channelNameValue) {
+            state.channelName = channelNameValue;
         }
     },
     actions: {
         setChannelInfoActions({ commit }, channelValue) {
             commit('setChannelInfo', channelValue);
+        },
+        setChannelNameInfoActions({ commit }, channelNameValue) {
+            commit('setChannelNameInfo', channelNameValue);
         }
     },
     getters: {
         getChannelId: state => state.channelId,
+        getChannelName: state => state.channelName,
     }
 }
 
