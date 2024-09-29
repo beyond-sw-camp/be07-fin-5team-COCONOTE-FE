9af5309df5e7a5a273855ca46d5b1906be988b1c
JUNG-EUN1997 <rro0628@gmail.com>
2024-09-29 12:21:44 +0900

    [STYLE] canvas view css

--- src/views/canvas/CanvasView.vue
+++ src/views/canvas/CanvasView.vue
@@ -1,30 +1,97 @@
 <template>
-  <v-app>
+  <div class="channelInsideContainer">
     <ChannelCommonMenu />
-    <h1>canvas View Test</h1>
-    <div>
-      <CanvasListComponent />
-      <CanvasDetailComponent />
+    <div class="channelInsideContentWrap">
+      <v-row class="canvasContatiner ma-0">
+        <v-col cols="2" class="canvasListContainer pa-0">
+          <v-list class="h-100">
+            <v-list-item prepend-icon="mdi-home">Home</v-list-item>
+
+            <!-- Users 그룹 -->
+            <v-list-group v-model="open[0]" value="Users">
+              <template v-slot:activator="{ props }">
+                <v-list-item v-bind="props" prepend-icon="mdi-account-circle">
+                  Users
+                </v-list-item>
+              </template>
+
+              <!-- Admin 그룹 -->
+              <v-list-group v-model="open[1]" value="Admin">
+                <template v-slot:activator="{ props }">
+                  <v-list-item v-bind="props"> Admin </v-list-item>
+                </template>
+
+                <v-list-item
+                  v-for="([title, icon], i) in admins"
+                  :key="i"
+                  :prepend-icon="icon"
+                >
+                  {{ title }}
+                </v-list-item>
+              </v-list-group>
+
+              <!-- Actions 그룹 -->
+              <v-list-group v-model="open[2]" value="Actions">
+                <template v-slot:activator="{ props }">
+                  <v-list-item v-bind="props"> Actions </v-list-item>
+                </template>
+
+                <v-list-item
+                  v-for="([title, icon], i) in cruds"
+                  :key="i"
+                  :prepend-icon="icon"
+                >
+                  {{ title }}
+                </v-list-item>
+              </v-list-group>
+            </v-list-group>
+          </v-list>
+        </v-col>
+        <v-col class="canvasDetailContainer pa-0">이얍 캔버스 내요옹</v-col>
+      </v-row>
     </div>
-  </v-app>
+  </div>
 </template>
 
 <script>
 import ChannelCommonMenu from "@/components/basic/ChannelCommonMenu.vue";
-import CanvasListComponent from "@/components/canvas/CanvasListComponent.vue";
-import CanvasDetailComponent from "@/components/canvas/CanvasDetailComponent.vue";
 
 export default {
   components: {
     ChannelCommonMenu,
-    CanvasListComponent,
-    CanvasDetailComponent,
   },
   data() {
-    return {};
+    return {
+      open: [true, false, false], // Users 그룹만 기본적으로 열림
+      admins: [
+        ["Management", "mdi-account-multiple-outline"],
+        ["Settings", "mdi-cog-outline"],
+      ],
+      cruds: [
+        ["Create", "mdi-plus-outline"],
+        ["Read", "mdi-file-outline"],
+        ["Update", "mdi-update"],
+        ["Delete", "mdi-delete"],
+      ],
+    };
   },
-  computed: {},
-  created() {},
-  methods: {},
 };
 </script>
+
+<style lang="scss">
+.canvasContatiner {
+  height: 100%;
+
+  .canvasListContainer {
+    display: flex;
+    flex-direction: column;
+    height: 100%;
+    > * {
+      background: #f7f7f7;
+    }
+    .v-list-item__content {
+      overflow: visible;
+    }
+  }
+}
+</style>
