498c1c1c955691e895f1f37f83712f7568473d9f
sseho <kindseho@naver.com>
2024-09-30 20:51:18 +0900

    [Feat] HC-153 main.js에 axios 요청시 토큰 자동 세팅

--- src/main.js
+++ src/main.js
@@ -9,8 +9,39 @@ import vuetify from './plugins/vuetify';
 import '@mdi/font/css/materialdesignicons.css';
 // import axios from 'axios';
 import store from './store/index.js';
+import axios from 'axios';
 
 const app = createApp(App);
+
+axios.interceptors.request.use(
+    (config) => {
+        const token = localStorage.getItem("accessToken");
+        if (token) {
+            config.headers["Authorization"] = `Bearer ${token}`;
+        }
+        return config;
+    },
+    (error) => {
+        return Promise.reject(error);
+    }
+);
+
+axios.interceptors.response.use(
+    response => response,
+    async error => {
+        const token = localStorage.getItem("accessToken");
+        if (error.response && error.response.status === 401 && token!=null) {
+            try {
+                localStorage.clear();
+                alert("토큰이 만료되었습니다 재로그인 해주세요!!")
+                window.location.href = "/login";
+            } catch (e) {
+                console.log(e.response);
+            }
+        }
+        return Promise.reject(error);
+    }
+);
 
 app.use(store);
 app.use(router);
