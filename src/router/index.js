ee6338bc38c4d12b1351bc5df16a08c9ac6fda14
Jiho Kim <jiho3634@gmail.com>
2024-09-30 10:08:29 +0900

    [FEAT] HC-149 토큰 저장

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,11 @@
 import {createRouter, createWebHistory} from 'vue-router';
 
 import HomePage from '@/views/HomePage.vue';
+import LoginPage from '@/views/LoginPage.vue';
+import OAuth2Success from '@/views/OAuth2Success.vue'; // OAuth2 성공 페이지 컴포넌트
+import YourComponent from '@/views/YourComponent.vue'; // 추가된 컴포넌트 경로
+
+
 import { canvasRouter } from './canvasRouter';
 import { threadRouter } from '@/router/threadRouter'
 import { channelRouter } from '@/router/channelRouter'
@@ -12,6 +17,21 @@ const routes = [
         path: '/',
         name: 'HOME',
         component: HomePage
+    },
+    {
+        path: '/login',
+        name: 'LOGIN',
+        component: LoginPage,
+    },
+    {
+        path: '/oauth2/success',
+        name: 'OAuth2Success',
+        component: OAuth2Success, // 컴포넌트 경로를 실제 파일로 변경해 주세요.
+    },
+    {
+        path: '/protected', // 원하는 경로 지정
+        name: 'ProtectedPage',
+        component: YourComponent, // 새로 추가한 컴포넌트
     },
     ...channelRouter,
     ...canvasRouter,
